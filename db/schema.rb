a146c31a3b9af37fa9b6295bea8cd615986a60c5
jvssvarma <varma.santosh@outlook.com>
2016-11-15 02:50:25 -0800

    Added Campaigns Scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161115104504) do
+ActiveRecord::Schema.define(version: 20161115104900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "campaigns", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "name"
+    t.string   "typeof"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_campaigns_on_user_id", using: :btree
+  end
 
   create_table "user_profiles", force: :cascade do |t|
     t.integer  "user_id"
@@ -41,5 +50,6 @@ ActiveRecord::Schema.define(version: 20161115104504) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "campaigns", "users"
   add_foreign_key "user_profiles", "users"
 end
